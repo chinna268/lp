07f31884d10855855540a6da05f3315a25f4d086
chinna268 <63144341+chinna268@users.noreply.github.com>
2020-11-02 10:51:31 +0530

    Add files via upload

---
+++ IPL.py
@@ -0,0 +1,5 @@
+import sports
+from pynotifier import Notification
+Matchinfo = sports.get_sport("CRICKET)
+Notification(title="IPL LIVE SCORE UPDATES")
+description=str(Matchinfo),duration=60).send()
